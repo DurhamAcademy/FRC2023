ce872527f959bff7e4034cffd241ae96361b40ed
25lid <94564177+25lid@users.noreply.github.com>
2023-01-09 17:19:00 -0500

    Jan 9 build

---
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -0,0 +1,11 @@
+package frc.robot
+
+import edu.wpi.first.wpilibj.XboxController
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController
+import frc.robot.subsystems.Drivetrain
+
+class RobotContainer {
+    val xbox = CommandXboxController(0)
+    val drivetrain = Drivetrain()
+    val
+}
