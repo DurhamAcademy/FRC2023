5cfe188e4ec2f2de61a35990ef41fab49b56eafb
Everett Wilber <71281043+a1cd@users.noreply.github.com>
2023-01-10 18:50:14 -0500

    Drivetrain and SwerveModule changes from many people
    Signed-off-by: a1cd <71281043+a1cd@users.noreply.github.com>

--- src/main/kotlin/frc/robot/subsystems/Drivetrain.kt
+++ src/main/kotlin/frc/robot/subsystems/Drivetrain.kt
@@ -1,59 +1,41 @@
 package frc.robot.subsystems
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX
-import com.ctre.phoenix.sensors.CANCoder
-import edu.wpi.first.math.controller.PIDController
-import edu.wpi.first.math.controller.ProfiledPIDController
+import edu.wpi.first.math.geometry.Translation2d
+import edu.wpi.first.math.kinematics.ChassisSpeeds
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics
 import edu.wpi.first.math.kinematics.SwerveModuleState
-import edu.wpi.first.math.trajectory.TrapezoidProfile
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints
+import frc.robot.Constants.BLDriveMotorId
+import frc.robot.Constants.BLTurnEncoderId
+import frc.robot.Constants.BLTurnMotorId
+import frc.robot.Constants.BRDriveMotorId
+import frc.robot.Constants.BRTurnEncoderId
+import frc.robot.Constants.BRTurnMotorId
+import frc.robot.Constants.FLDriveMotorId
+import frc.robot.Constants.FLTurnEncoderId
+import frc.robot.Constants.FLTurnMotorId
+import frc.robot.Constants.FRDriveMotorId
+import frc.robot.Constants.FRTurnEncoderId
+import frc.robot.Constants.FRTurnMotorId
 
 class Drivetrain : SubsystemBase() {
-    class SwerveModule(
-        driveMotorId: Int,
-        turnMotorId: Int,
-        encoderId: Int,
-    ) {
-        private val driveMotor = WPI_TalonFX(driveMotorId)
-        private val turnMotor = WPI_TalonFX(turnMotorId)
-        private val turnEncoder = CANCoder(encoderId)
-
-        //should adjust these gains or characterize since they are a little slow
-        val DRIVE_P = 0.1
-        val DRIVE_I = 0.0
-        val DRIVE_D = 0.0
-        val drivePid = ProfiledPIDController(
-            DRIVE_P,
-            DRIVE_I,
-            DRIVE_D,
-            Constraints(2.0,2.0 )// TODO: Fix these
-        )
-        //should adjust these gains or characterize since they are a little slow
-        val ANGLE_P = 0.01
-        val ANGLE_I = 0.0
-        val ANGLE_D = 0.0
-        val anglePid = ProfiledPIDController(
-            ANGLE_P,
-            ANGLE_I,
-            ANGLE_D,
-            Constraints(
-                3.0,
-                3.0
-            )// TODO: Fix these
-        ).apply {
-            enableContinuousInput(-Math.PI, Math.PI)
+    val FL = SwerveModule(FLDriveMotorId, FLTurnMotorId, FLTurnEncoderId, Translation2d()) // FIXME: add actual positon
+    val FR = SwerveModule(FRDriveMotorId, FRTurnMotorId, FRTurnEncoderId, Translation2d()) // FIXME: add actual positon
+    val BL = SwerveModule(BLDriveMotorId, BLTurnMotorId, BLTurnEncoderId, Translation2d()) // FIXME: add actual positon
+    val BR = SwerveModule(BRDriveMotorId, BRTurnMotorId, BRTurnEncoderId, Translation2d()) // FIXME: add actual positon
+    val modules = listOf(FL, FR, BL, BR)
+    val kinematics = SwerveDriveKinematics(
+        *modules.map { it.position }.toTypedArray()
+    )
+    fun drive(chassisSpeeds: ChassisSpeeds) {
+        val goals: Array<SwerveModuleState> = kinematics.toSwerveModuleStates(chassisSpeeds)
+        modules[0].setpoint = goals[0]
+        modules[1].setpoint = goals[1]
+        for (i in goals.indices) {
+            modules[i].setpoint = goals[i]
         }
-
-        var setpoint = SwerveModuleState()
-
-        fun move() {
-
-        }
-
-
-        fun periodic() {
-
+        goals.forEachIndexed { index, goal ->
+            modules[index].setpoint = goal
         }
     }
 }
