5285881890f19ba61adc0df96689bd9b1a764afc
okCookie <26wilber@da.org>
2023-01-10 18:46:54 -0500

    Changes by
    Signed-off-by: a1cd <71281043+a1cd@users.noreply.github.com>

---
+++ src/main/kotlin/frc/robot/Constants.kt
@@ -0,0 +1,18 @@
+package frc.robot
+
+object Constants {
+    const val FLDriveMotorId = 0
+    const val FRDriveMotorId = 0
+    const val BLDriveMotorId = 0
+    const val BRDriveMotorId = 0
+
+    const val FLTurnMotorId = 0
+    const val FRTurnMotorId = 0
+    const val BLTurnMotorId = 0
+    const val BRTurnMotorId = 0
+
+    const val FLTurnEncoderId = 0
+    const val FRTurnEncoderId = 0
+    const val BLTurnEncoderId = 0
+    const val BRTurnEncoderId = 0
+}
