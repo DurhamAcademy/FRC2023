b31c206803094703a7731f4aae2cfe769286b146
KP <86213869+Kanishk-Pandey@users.noreply.github.com>
2023-02-19 14:52:20 -0500

    Created Intake Class inheriting subsystembase, the motor is phoenix because SparkMax is being beta. Will fix and change to SparkMax Later.

--- src/main/kotlin/frc/robot/subsystems/Intake.kt
+++ src/main/kotlin/frc/robot/subsystems/Intake.kt
@@ -4,7 +4,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.math.system.plant.DCMotor
 class Intake : SubsystemBase() {
-
+//Chang to SparkMax after the WPILib for it starts working Phoenix is a temporary patch.
 var motor = WPI_TalonFX(12)
 
 
