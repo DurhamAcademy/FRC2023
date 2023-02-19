12523717e22c4d06880eb20e38770a1e4985a117
KP <86213869+Kanishk-Pandey@users.noreply.github.com>
2023-02-19 13:49:00 -0500

    Created Intake Class

---
+++ src/main/kotlin/frc/robot/subsystems/Intake.kt
@@ -0,0 +1,11 @@
+package frc.robot.subsystems
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import edu.wpi.first.math.system.plant.DCMotor
+import frc.*;
+class Intake : SubsystemBase() {
+    object Intake : SubsystemBase() {
+        //var deployed
+        //  }
+    }
+}
