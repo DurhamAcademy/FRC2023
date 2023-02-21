f9b9fa90bddfc854e24e69a33780e6dce0896fb1
Aarav <peacefulprakashaarav@gmail.com>
2023-02-21 08:37:59 -0500

    Created updated function for both motors to set or get the speed of said motor.

--- src/main/kotlin/frc/robot/subsystems/Intake.kt
+++ src/main/kotlin/frc/robot/subsystems/Intake.kt
@@ -4,13 +4,24 @@ import com.revrobotics.CANSparkMax
 import com.revrobotics.CANSparkMaxLowLevel
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
+import frc.robot.controls.ControlScheme
+
+
 class Intake(
 
 ): SubsystemBase() {
     private val deployMotor = CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless)
     private val spinningMotor = CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless)
+    var spinMotorSpeed: Double
+        get() = spinningMotor.get()
+        set(value) {
+            spinningMotor.set(value)
+        }
+    var deployMotorSpeed: Double
+        get() = deployMotor.get()
+        set(value) {
+            deployMotor.set(value)
+        }
 
-    private fun spinMotor(x: Double) {
-        spinningMotor.set(x)
-    }
+
 }
