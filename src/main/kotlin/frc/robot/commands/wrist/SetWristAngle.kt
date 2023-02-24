167c1df6a9d5962adeb5b342335fc39b4954adaa
Ryan Bauroth <25bauroth@da.org>
2023-02-24 16:37:24 -0500

    added if statements and math to SetWristAngle.kt

--- src/main/kotlin/frc/robot/commands/wrist/SetWristAngle.kt
+++ src/main/kotlin/frc/robot/commands/wrist/SetWristAngle.kt
@@ -1,13 +1,16 @@
 package frc.robot.commands.wrist
 
 import edu.wpi.first.wpilibj2.command.CommandBase
+import frc.robot.subsystems.Arm
 import frc.robot.subsystems.Wrist
 
 class SetWristAngle(
     private val wrist: Wrist,
+    private val arm: Arm,
 ) : CommandBase() {
     init {
         addRequirements(wrist)
+        addRequirements(arm)
     }
 
     override fun initialize() {
@@ -15,6 +18,18 @@ class SetWristAngle(
     }
 
     override fun execute() {
+        if(arm.armPosition < -20){
+            wrist.setPosition((90 + arm.armPosition) * -1)
+        }
+        else if(arm.armPosition > 20){
+            wrist.setPosition(90 - arm.armPosition)
+        }
+        else if(arm.armPosition > 0){
+            wrist.setPosition(arm.armPosition * -3.5)
+        }
+        else{
+            wrist.setPosition(arm.armPosition * 3.5)
+        }
     }
 
     override fun end(interrupted: Boolean) {
