f9caa73ac1a97530c4638ee37529cf2730eb7a97
a1cd <71281043+a1cd@users.noreply.github.com>
2023-02-27 00:46:20 -0500

    added RobotContainer commands
    Signed-off-by: a1cd <71281043+a1cd@users.noreply.github.com>

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -2,15 +2,19 @@ package frc.robot
 
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
 import frc.robot.commands.MoveToPosition
+import frc.robot.commands.manipulator.GrabConeCommand
+import frc.robot.commands.manipulator.HoldConeCommand
 import frc.robot.controls.ControlScheme
 import frc.robot.controls.DefaultControlScheme
 import frc.robot.subsystems.Drivetrain
+import frc.robot.subsystems.Manipulator
 
 class RobotContainer {
     val xbox = CommandXboxController(0)
     val controlScheme: ControlScheme = DefaultControlScheme(xbox)
 
     var cameraWrapper = PhotonCameraWrapper()
+    val manipulator = Manipulator(cameraWrapper)
 
     @Suppress("unused")
     val drivetrain = Drivetrain(controlScheme, cameraWrappers = listOf(cameraWrapper))
@@ -29,6 +33,36 @@ class RobotContainer {
             testGoToZeroZero
                 .whileTrue(
                     MoveToPosition(drivetrain, 0.0, 0.0, 0.0)
+                )
+
+            // assign the open manipulator trigger to the command that
+            // opens the manipulator
+            openManipulator
+                .onTrue(
+                    OpenManipulatorCommand(manipulator)
+                )
+
+            // assign the close manipulator trigger to the command that
+            // closes the manipulator
+            closeManipulator
+                .onTrue(
+                    CloseManipulatorCommand(manipulator)
+                )
+
+            // assign the grab cone trigger to the command that
+            // grabs a cone
+            grabCone
+                .whileTrue(
+                    GrabConeCommand(manipulator)
+                        .until { manipulator.objectType == GamePiece.cone }
+                        .andThen(HoldConeCommand(manipulator))
+                )
+
+            // assign the hold cone trigger to the command that
+            // holds a cone
+            holdCone
+                .whileTrue(
+                    HoldConeCommand(manipulator)
                 )
         }
     }
