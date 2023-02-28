12d8fc52299b9eefbf5525ee1f729ed5cefc3393
Aarav <peacefulprakashaarav@gmail.com>
2023-02-27 22:50:04 -0500

    added a setPositionMid command, doesnt have control scheme

--- src/main/kotlin/frc/robot/commands/alltogether/setPositionMid.kt
+++ src/main/kotlin/frc/robot/commands/alltogether/setPositionMid.kt
@@ -6,7 +6,7 @@ import frc.robot.subsystems.Elevator
 import frc.robot.subsystems.Arm
 import frc.robot.controls.ControlScheme
 
-class setPositionHigh(
+class setPositionMid(
     val elevator: Elevator,
     val arm: Arm,
     val controlScheme: ControlScheme,
@@ -17,15 +17,15 @@ class setPositionHigh(
     }
 
     override fun execute() {
-        // change this to 48 inches(idk how)
+        // change this to 34 inches(idk how)
         //I think i did but double check this
-        elevator.setpoint = (Constants.Elevator.limits.bottomLimit + 48)
+        elevator.setpoint = (Constants.Elevator.limits.bottomLimit + 34)
 
         arm.setArmPosition(1.4)
     }
 
     //double check the arm position thing
     override fun isFinished(): Boolean =
-        elevator.height == 48.0 && arm.armPosition == 1.4
+        elevator.height == 34.0 && arm.armPosition == 1.4
 
 }
