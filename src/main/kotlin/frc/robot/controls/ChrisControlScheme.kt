b6b412a4653d9dbcfc357356e443890379958f5e
a1cd <71281043+a1cd@users.noreply.github.com>
2023-03-14 13:49:54 -0400

    proper elevator zeroing

--- src/main/kotlin/frc/robot/controls/ChrisControlScheme.kt
+++ src/main/kotlin/frc/robot/controls/ChrisControlScheme.kt
@@ -1,14 +1,16 @@
 package frc.robot.controls
 
+import edu.wpi.first.math.MathUtil
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController
 
-class ChrisControlScheme: ControlScheme() {
-    override val xbox: CommandXboxController?
-        get() = TODO("Not yet implemented")
+class ChrisControlScheme(
+    xboxNum: Int = 0
+) : ControlScheme() {
+    override val xbox = CommandXboxController(xboxNum)
     override val rotation: Double
-        get() = TODO("Not yet implemented")
+        get() = MathUtil.applyDeadband(xbox.rightX.coerceIn(-1.0, 1.0), 0.05)
     override val strafe: Double
-        get() = TODO("Not yet implemented")
+        get() = MathUtil.applyDeadband(xbox.leftX.coerceIn(-1.0, 1.0), 0.05)
     override val forward: Double
-        get() = TODO("Not yet implemented")
+        get() = MathUtil.applyDeadband(xbox.leftY.coerceIn(-1.0, 1.0), 0.05)
 }
