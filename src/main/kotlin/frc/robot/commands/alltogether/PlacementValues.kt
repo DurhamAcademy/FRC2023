d7e74362cb664d6db8c08a00dcfa5154bdc7f7b2
Ryan Bauroth <25bauroth@da.org>
2023-03-17 12:00:26 -0400

    should work just needs a look-over

--- src/main/kotlin/frc/robot/commands/alltogether/PlacementValues.kt
+++ src/main/kotlin/frc/robot/commands/alltogether/PlacementValues.kt
@@ -9,14 +9,11 @@ import edu.wpi.first.math.util.Units.inchesToMeters
  * @param elevatorHeight the height the elevator needs to go to in meters
  * @param armRotation the rotation the arm needs to be at as a Rotation2d
  */
-enum class IOLevel(val elevatorHeight: Double, val armRotation: Rotation2d){
-    HighCone(frc.robot.constants.elevator.limits.topLimit - inchesToMeters(2.0), Rotation2d.fromRadians(1.32)),
-    HighCube(frc.robot.constants.elevator.limits.topLimit - inchesToMeters(2.0), Rotation2d.fromRadians(1.32)),
-    MidCone(inchesToMeters(38.0), Rotation2d.fromRadians(1.32)),
-    MidCube(inchesToMeters(38.0), Rotation2d.fromRadians(1.32)),
-    LowCone(frc.robot.constants.elevator.limits.topLimit, Rotation2d.fromRadians(2.61)),
-    LowCube(frc.robot.constants.elevator.limits.topLimit, Rotation2d.fromRadians(2.61)),
-    HumanPlayerSlider(1.3 - inchesToMeters(11.0), Rotation2d.fromRadians(1.4)),
-    Idle(frc.robot.constants.elevator.limits.bottomLimit, Rotation2d.fromRadians(0.0)),
-    FloorIntake(frc.robot.constants.elevator.limits.bottomLimit + inchesToMeters(1.0), Rotation2d.fromRadians(1.81))
+enum class IOLevel(val coneElevatorHeight: Double, val coneArmRotation: Rotation2d, val cubeElevatorHeight: Double, val cubeArmRotation: Rotation2d){
+    High(frc.robot.constants.elevator.limits.topLimit - inchesToMeters(2.0), Rotation2d.fromRadians(1.32), frc.robot.constants.elevator.limits.topLimit - inchesToMeters(2.0), Rotation2d.fromRadians(1.32)),
+    Mid(inchesToMeters(38.0), Rotation2d.fromRadians(1.32),inchesToMeters(38.0), Rotation2d.fromRadians(1.32)),
+    Low(frc.robot.constants.elevator.limits.topLimit, Rotation2d.fromRadians(2.61),frc.robot.constants.elevator.limits.topLimit, Rotation2d.fromRadians(2.61)),
+    HumanPlayerSlider(1.3 - inchesToMeters(11.0), Rotation2d.fromRadians(1.4), 1.3 - inchesToMeters(11.0), Rotation2d.fromRadians(1.4)),
+    Idle(frc.robot.constants.elevator.limits.bottomLimit, Rotation2d.fromRadians(0.0), frc.robot.constants.elevator.limits.bottomLimit, Rotation2d.fromRadians(0.0)),
+    FloorIntake(frc.robot.constants.elevator.limits.bottomLimit + inchesToMeters(1.0), Rotation2d.fromRadians(1.81), frc.robot.constants.elevator.limits.bottomLimit + inchesToMeters(1.0), Rotation2d.fromRadians(1.81))
 }
--- src/main/kotlin/frc/robot/commands/alltogether/SetSubsystemPosition.kt
+++ src/main/kotlin/frc/robot/commands/alltogether/SetSubsystemPosition.kt
@@ -5,36 +5,56 @@ import frc.robot.RobotContainer
 import frc.robot.constants.FieldConstants
 import frc.robot.subsystems.Arm
 import frc.robot.subsystems.Elevator
+import frc.robot.utils.GamePiece
+import kotlin.math.absoluteValue
 import kotlin.math.sin
 
 class SetSubsystemPosition (
     val elevator: Elevator,
     val arm: Arm,
     val level: IOLevel,
+    val wantedObject: GamePiece,
 
     val stopAtEnd: Boolean = true
 ) : CommandBase() {
-    constructor(robotContainer: RobotContainer, level: IOLevel) : this(
+    constructor(robotContainer: RobotContainer, level: IOLevel, wantedObject: GamePiece) : this(
         robotContainer.elevator,
         robotContainer.arm,
-        level
+        level,
+        wantedObject,
     )
+    val topLimit = FieldConstants.heightLimit
+    val bottomLimit = 0.1
+    val armLength = frc.robot.constants.arm.length
+    var goalArmPosition = 0.0
 
     init {
         addRequirements(arm)
         addRequirements(elevator)
     }
     override fun execute() {
-        arm.setArmPosition(level.armRotation)
-        val topLimit = FieldConstants.heightLimit
-        val bottomLimit = 0.1
-        val armLength = frc.robot.constants.arm.length
-        // use arm angle to determine elevator height
-        val armAngle = arm.armPosition
-        val armHeight = armLength * sin(armAngle)
-        val elevatorMaxHeight = topLimit - armHeight
-        elevator.setpoint = level.elevatorHeight
-            .coerceIn(bottomLimit, elevatorMaxHeight)
+        if(wantedObject == GamePiece.cube){
+            arm.setArmPosition(level.cubeArmRotation)
+            goalArmPosition = level.cubeArmRotation.degrees
+            // use arm angle to determine elevator height
+            val armAngle = arm.armPosition
+            val armHeight = armLength * sin(armAngle)
+            val elevatorMaxHeight = topLimit - armHeight
+            elevator.setpoint = level.cubeElevatorHeight
+                .coerceIn(bottomLimit, elevatorMaxHeight)
+        }
+        else{
+            arm.setArmPosition(level.coneArmRotation)
+            goalArmPosition = level.coneArmRotation.degrees
+            // use arm angle to determine elevator height
+            val armAngle = arm.armPosition
+            val armHeight = armLength * sin(armAngle)
+            val elevatorMaxHeight = topLimit - armHeight
+            elevator.setpoint = level.coneElevatorHeight
+                .coerceIn(bottomLimit, elevatorMaxHeight)
+        }
     }
+    override fun isFinished(): Boolean = stopAtEnd && (arm.armPosition - goalArmPosition).absoluteValue < 0.1
+
 }
 
