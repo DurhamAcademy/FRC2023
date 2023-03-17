9d278ff8371ffa682c5fe1ea83f7dac6244c9311
KP <86213869+Kanishk-Pandey@users.noreply.github.com>
2023-03-16 21:25:42 -0400

    Fixed up Throw Command - it works

--- src/main/kotlin/frc/robot/commands/manipulator/Throw.kt
+++ src/main/kotlin/frc/robot/commands/manipulator/Throw.kt
@@ -2,25 +2,25 @@ package frc.robot.commands.manipulator
 
 import edu.wpi.first.wpilibj2.command.CommandBase
 import frc.robot.RobotContainer
+import frc.robot.constants.manipulator
 import frc.robot.subsystems.Manipulator
 import frc.robot.utils.GamePiece
 import frc.robot.utils.GamePiece.*
 
 class Throw (
     private val manipulator: Manipulator,
-    private val speed:Double,
-    val gamePiece: () -> GamePiece
+    val gamePiece: ()->GamePiece
 ) : CommandBase(){
     init {
         addRequirements(manipulator)
     }
 
     override fun execute(){
-        when(gamePiece()) {
-            cone -> SetManipulatorSpeed(manipulator, -0.2)
-            cube -> SetManipulatorSpeed(manipulator, -0.15)
-            unknown -> SetManipulatorSpeed(manipulator, -0.15)
-            none -> SetManipulatorSpeed(manipulator, -0.15)
+        manipulator.motorPercentage = when (gamePiece()) {
+            cone -> -0.2
+            cube -> -0.15
+            unknown -> -0.15
+            none -> -0.15
         }
     }
 }
