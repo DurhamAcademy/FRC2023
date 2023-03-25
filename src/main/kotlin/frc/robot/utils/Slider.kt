d69034844039046cf4c2e60d83dfd4444a2c1e69
Kanishk-Pandey <kanishkpandey0407@gmail.com>
2023-03-25 10:29:01 -0400

    fixed hpslider auto align
    Signed-off-by: Kanishk-Pandey <kanishkpandey0407@gmail.com>

--- src/main/kotlin/frc/robot/utils/Slider.kt
+++ src/main/kotlin/frc/robot/utils/Slider.kt
@@ -3,5 +3,5 @@ package frc.robot.utils
 import edu.wpi.first.math.util.Units.inchesToMeters
 
 enum class Slider(val fieldYValue: Double) {
-    close(6.0 - 4.0), far(7.3 + inchesToMeters(4.0))
+    close(6.0 - inchesToMeters(4.0)), far(7.3 + inchesToMeters(4.0))
 }
