c8292aeb849fd8d5937adc57f24bd54edebca3af
KP <86213869+Kanishk-Pandey@users.noreply.github.com>
2023-03-31 08:15:37 -0400

    removed redundant stuff

--- src/main/kotlin/frc/robot/commands/pathing/NoVisionAuto.kt
+++ src/main/kotlin/frc/robot/commands/pathing/NoVisionAuto.kt
@@ -41,47 +41,4 @@ class NoVisionAuto(
             )
             .withTimeout(8.0)
     }
-
-
-    fun pathBlue(robotContainer: RobotContainer): Command {
-        return SetSubsystemPosition(
-            robotContainer,
-            { IOLevel.High },
-            { GamePiece.cone },
-            true
-        )
-            .andThen(
-                Throw(
-                    robotContainer.manipulator,
-                    { GamePiece.cone },
-                    { PlacementLevel.Level3 }
-                )
-                    .withTimeout(1.5))
-            .andThen(
-                DriveCommand(
-                    robotContainer.drivetrain,
-                    { 1.0 },
-                    { 0.0 },
-                    { 0.0 },
-                    false,
-                    { Translation2d(0.0, 0.0) }
-                )
-                    .withTimeout(4.0)
-                    .alongWith(
-                        SetSubsystemPosition(
-                            robotContainer,
-                            { IOLevel.Idle },
-                            { GamePiece.cone },
-                            true
-                        )
-                            .andThen(
-                                ZeroElevatorAndIdle(
-                                    robotContainer.elevator,
-                                    robotContainer.arm
-                                )
-                            )
-                    )
-                    .alongWith(IdleIntake(robotContainer.intake, { GamePiece.cube }))
-            )
-    }
 }
